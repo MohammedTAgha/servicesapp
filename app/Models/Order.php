257e760e3645c9cecffb5516c370e8f4b9bc5272
MOHAMMED T AGHA <36562531+MohammedTAgha@users.noreply.github.com>
2025-02-07 16:07:22 +0200

    init

---
+++ app/Models/Order.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+    protected $fillable = ['service_id', 'user_id', 'status', 'payment', 'date', 'location', 'notes'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function service()
+    {
+        return $this->belongsTo(Service::class);
+    }
+
+}
---
+++ database/migrations/2025_02_05_150834_create_payments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Foreign key to users
+            $table->foreignId('service_id')->constrained()->onDelete('cascade'); // Foreign key to services
+            $table->integer('rating'); // Rating (e.g., 1 to 5)
+            $table->text('comment')->nullable(); // Review comment
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
